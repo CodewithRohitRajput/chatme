69e459a79cf919cab330ba05fcd6fa2030d3688a
CodewithRohitRajput <rohitsinghrajput2k24@gmail.com>
2026-02-19 22:31:01 +0530

    add cookies and completed the route of users

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,12 +1,15 @@
 import User from '../models/user.js'
-import express from 'express'
+import type { Request, Response } from 'express'
 import dotenv from 'dotenv'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 dotenv.config()
 
-const router = express.Router()
+const access_token_secret = process.env.ACCESS_TOKEN_SECRET
+if(!access_token_secret) throw new Error('SECRET is not present')
 
-router.post("/signup", async (req, res) => {
+
+export const signup = async (req: Request, res: Response) => {
     const {username, email, password} = req.body;
 
     const alreadyUsername = await User.findOne({username});
@@ -15,11 +18,22 @@ router.post("/signup", async (req, res) => {
     if(alreadyEmail) return res.status(409).json({message : "email already exists"})
     const hashedPass = await bcrypt.hash(password,10)
     const newUser = await User.create({username, email, password : hashedPass})
+    const token = await jwt.sign(
+        {userId : newUser._id},
+        access_token_secret,
+        {expiresIn : "1d"}
+    );
+    res.cookie("token", token, {
+        httpOnly : true,
+        secure : false,
+        sameSite : "lax",
+
+    })
     return res.status(201).json({message : "user created successfully"})
-})
+}
 
 
-router.post("/login", async (req, res) => {
+export const login = async (req: Request, res: Response) => {
 
     const {email, password} = req.body;
     const isUser = await User.findOne({email}) as { password: string } | null;
@@ -28,6 +42,4 @@ router.post("/login", async (req, res) => {
     if(!chkPass) return res.status(401).json({message : "password is wrong"})
     return res.status(200).json({message : "user loggedIn"})
 
-})
-
-export default router;
+}
--- server/server.ts
+++ server/server.ts
@@ -2,18 +2,19 @@ import express from 'express'
 import cors from 'cors'
 import type { Request, Response } from 'express';
 import ConnectDB from './config/db.js';
+import cookieParser from 'cookie-parser'
+import userRouter from './routes/userRoute.js';
 const app = express()
 const port = 8000;
 
 
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 
 ConnectDB()
 
-app.get("/", (req : Request , res : Response)=>{
-    res.send("hey backend in working dude !!!")
-})
+app.use("/users", userRouter)
 
 app.listen(port, ()=>{
     console.log(`server is running port ${port}`)
